2f77562893dd78aea111765dc9b63d69da34a23d
Pavel Kudlanov <pkudlanov@yahoo.com>
2019-05-31 14:10:17 -0400

    we completed searchcomponent functionality and accounted for a blank search

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,7 @@ class App extends Component {
         dom.insertBefore(headerDom, main);
 
         const search = new Search();
-        main.appendChild(search.render());
+        main.appendChild(search.render())
 
         const paging = new Paging({ totalCount: 0 });
         main.appendChild(paging.render());
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,6 +1,34 @@
 import Component from './Component.js';
+import hashStorage from '../services/hash-storage.js';
 
 class Search extends Component {
+
+    render() {
+        const form = this.renderDOM();
+        const input = form.querySelector('input');
+
+        form.addEventListener('submit', event => {
+            event.preventDefault();
+            hashStorage.set({ pokemon: input.value, page: 1 });
+        });
+
+        function setInputFromHash() {
+            const queryProps = hashStorage.get();
+            if(!input.value){
+                input.value = '';
+            } else {
+                input.value = queryProps.pokemon;
+            }
+        }
+
+        setInputFromHash();
+
+        window.addEventListener('hashchange', () => {
+            setInputFromHash();
+        });
+
+        return form;
+    }
 
     renderTemplate() {
         return /*html*/`
